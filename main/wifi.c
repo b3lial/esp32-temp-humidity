03b705d273898169b7efafb73172cf7a31c7166e
Dr. Christian Ammann <christian.ammann@phobosys.de>
2022-06-17 00:00:47 +0200

    wifi cleanup, added menuconfig for wifi credentials

--- main/wifi.c
+++ main/wifi.c
@@ -1,9 +1,8 @@
 #include "cloudsensor.h"
 
+static const char *TAG = "wifi handler";
 static void event_handler(void *arg, esp_event_base_t event_base,
                           int32_t event_id, void *event_data);
-
-static const char *TAG = "wifi handler";
 
 static EventGroupHandle_t s_wifi_event_group;
 
@@ -11,37 +10,33 @@ void start_wifi()
 {
     ESP_LOGI(TAG, "starting wifi");
 
+    // init wifi subsystem
     ESP_ERROR_CHECK(esp_netif_init());
     ESP_ERROR_CHECK(esp_event_loop_create_default());
     esp_netif_create_default_wifi_sta();
 
+    // configure wifi credentials
     wifi_init_config_t cfg = WIFI_INIT_CONFIG_DEFAULT();
     ESP_ERROR_CHECK(esp_wifi_init(&cfg));
-
     wifi_config_t wifi_config = {
         .sta = {
-            .ssid = "DEFAULT_SSID",
-            .password = "DEFAULT_PWD",
-            /* Setting a password implies station will connect to all security modes including WEP/WPA.
-             * However these modes are deprecated and not advisable to be used. Incase your Access point
-             * doesn't support WPA2, these mode can be enabled by commenting below line */
-            .threshold.authmode = DEFAULT_AUTHMODE,
-
+            .ssid = CONFIG_CLOUDSENSOR_WIFI_SSID,
+            .password = CONFIG_CLOUDSENSOR_WIFI_PASSWORD,
+            .threshold.authmode = WIFI_AUTH_WPA2_PSK,
             .pmf_cfg = {
                 .capable = true,
                 .required = false},
         },
     };
+
+    // apply config
     ESP_ERROR_CHECK(esp_wifi_set_mode(WIFI_MODE_STA));
     ESP_ERROR_CHECK(esp_wifi_set_config(ESP_IF_WIFI_STA, &wifi_config));
 
+    // register event handler and start wifi connection process
     s_wifi_event_group = xEventGroupCreate();
     esp_event_handler_instance_t instance_any_id;
     esp_event_handler_instance_t instance_got_ip;
-
-    // Get wifi config
-    //wifi_config_t wifi_config;
-    //ESP_ERROR_CHECK(esp_wifi_get_config(ESP_IF_WIFI_STA, &wifi_config));
     ESP_ERROR_CHECK(esp_event_handler_instance_register(WIFI_EVENT,
                                                         ESP_EVENT_ANY_ID,
                                                         &event_handler,
@@ -54,8 +49,7 @@ void start_wifi()
                                                         &instance_got_ip));
     ESP_ERROR_CHECK(esp_wifi_start());
 
-    /* Waiting until either the connection is established (WIFI_CONNECTED_BIT) or connection failed for the maximum
-     * number of re-tries (WIFI_FAIL_BIT). The bits are set by event_handler() (see above) */
+    // wait until connection was successful
     EventBits_t bits = xEventGroupWaitBits(s_wifi_event_group,
                                            WIFI_CONNECTED_BIT | WIFI_FAIL_BIT,
                                            pdFALSE,
@@ -86,6 +80,7 @@ static void event_handler(void *arg, esp_event_base_t event_base,
     if (event_base == WIFI_EVENT && event_id == WIFI_EVENT_STA_START)
     {
         esp_wifi_connect();
+        ESP_LOGI(TAG, "trying to connect to the AP");
     }
     else if (event_base == WIFI_EVENT && event_id == WIFI_EVENT_STA_DISCONNECTED)
     {
